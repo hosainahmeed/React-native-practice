0480551c604aadb151845bc66d4efbb880022c04
hosain ali <hosainahmed534745@gmail.com>
2025-05-28 14:31:59 +0600

    try to make card with flatlist view text

---
+++ component/About.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const About = () => {
+  return (
+    <View>
+      <Text style={styles.text}>About</Text>
+    </View>
+  );
+};
+
+export default About;
+
+const styles = StyleSheet.create({
+    text:{
+        color:'red',
+        fontSize:20,
+        fontWeight:'bold',
+    },
+})
---
+++ component/Contact.jsx
@@ -0,0 +1,17 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const Contact = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Contact</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Contact;
+
+const styles = StyleSheet.create({});
